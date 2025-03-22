a20737a36d6e0fbad3cf22cba33cc1e3569ad698
mac <mac@Mac.lan>
2025-03-22 21:50:45 +0700

    fix3

--- api/serializers/task_serializer.py
+++ api/serializers/task_serializer.py
@@ -1,8 +1,8 @@
-# # api/serializers/task_serializer.py
-# from rest_framework import serializers
-# from api.models.task import Task
+# api/serializers/task_serializer.py
+from rest_framework import serializers
+from api.models.task import Task
 
-# class TaskSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Task
-#         fields = ['task_id', 'task_name', 'description', 'status', 'deadline', 'assignee', 'project']
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['task_id', 'task_name', 'description', 'status', 'deadline', 'assignee', 'project']
