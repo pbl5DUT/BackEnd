81d6927daa87b5ab1d14355afabb7017415d509e
mac <mac@Mac.lan>
2025-03-22 16:40:11 +0700

    all render

--- backend/settings.py
+++ backend/settings.py
@@ -25,10 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o3!xixv06c6r_kx!1^1l4elu9$18jwxk2n14ka%shhlq#^s5y('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
+
+DEBUG=False
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 # Application definition
 
